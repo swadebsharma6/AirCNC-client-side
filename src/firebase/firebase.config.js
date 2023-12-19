4ee9fd9481b56168db98b02d5f12eae630c3a17b
swadebsharma6 <swadebriva2012@gmail.com>
2023-12-20 01:16:41 +0600

    added files

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGOlbNtb3rvskYRD-ze9gHLMB5EPxTaos",
+  authDomain: "aircnc-ce5b1.firebaseapp.com",
+  projectId: "aircnc-ce5b1",
+  storageBucket: "aircnc-ce5b1.appspot.com",
+  messagingSenderId: "696838969399",
+  appId: "1:696838969399:web:b46d30ee8e16cd239c07b2"
+};
+
+
+export const app = initializeApp(firebaseConfig)
+
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,9 @@
+const Main = () => {
+  return (
+    <div>
+      <h1 className="text-blue-500">Welcome TO AirCNC</h1>
+    </div>
+  )
+}
+
+export default Main
